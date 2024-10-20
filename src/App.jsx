17acb4b6bd13dc9ec8f3b18eef3abcd09e5cccc8
stanislav-nedelchev <162455551+stanislav-nedelchev@users.noreply.github.com>
2024-10-20 15:45:45 +0300

    fixed search form

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,80 @@
 import './App.css';
+import { findImages } from './api/api';
+import { useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
-import Profile from './components/Profile/Profile.jsx';
-import FriendList from './components/FriendList/FriendList.jsx';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory.jsx';
-
-import userData from './data/userData.json';
-import friends from './data/friends.json';
-import transactions from './data/transactions.json';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import ImageModal from './components/ImageModal/ImageModal';
+import Loader from './components/Loader/Loader';
+import SearchBar from './components/SearchBar/SearchBar';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+
+    async function fetchImages() {
+      setIsLoading(true);
+      try {
+        const data = await findImages(query, page);
+        console.log(data);
+        setImages(previmages => [...previmages, ...data.results]);
+        setTotalPages(data.total_pages);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchImages();
+  }, [query, page]);
+
+  const openModal = image => {
+    setModalImage(image);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalImage(null);
+    setIsOpen(false);
+  };
+
+  const handleSubmit = query => {
+    setQuery(query);
+    setImages([]);
+  };
+
+  const handleClick = () => {
+    setPage(prevpage => prevpage + 1);
+  };
+
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <div>
+        <SearchBar onSubmit={handleSubmit} />
+        <ImageGallery images={images} openModal={openModal} />
+        <ImageModal
+          openModal={modalIsOpen}
+          imageModal={modalImage}
+          closeModal={closeModal}
+        />
+        {page < totalPages && <LoadMoreBtn onClick={handleClick} />}
+        {isLoading && <Loader />}
+        {errorMessage && <ErrorMessage />}
+        <Toaster toast={toast} />
+      </div>
     </>
   );
 }
